6ee7c18acd92f2315edbfb2b010cecce61c446ec
tyler-bartschi <tylerabartschi@gmail.com>
2025-03-02 21:09:12 -0700

    Made an AuthState class

---
+++ src/login/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./login.css";
+
+import { AuthState } from "./AuthState";
+
+export function Login() {
+    return (
+        <main>
+            <div>Login component</div>
+        </main>
+    );
+}
