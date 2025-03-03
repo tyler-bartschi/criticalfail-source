6b303397ea6b6c3216b01b58d59e227946a58ef9
tyler-bartschi <tylerabartschi@gmail.com>
2025-03-02 22:02:49 -0700

    Added basic styling and animation

--- src/app.jsx
+++ src/app.jsx
@@ -5,15 +5,24 @@ import './app.css';
 
 import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
 import {Login} from './login/login';
+import {AuthState} from './login/AuthState';
 
 export default function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || "");
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
     return (
         <BrowserRouter>
-            <header></header>
-            <Routes>
-                <Route path="/" element={<Login />} exact />
-            </Routes>
-            <footer></footer>
+            <div className="body">
+                {authState === AuthState.Authenticated && (
+                    <header>This is authenticated header placeholder</header>
+                )}
+                <Routes>
+                    <Route path="/" element={<Login />} exact />
+                </Routes>
+                <footer></footer>
+            </div>
         </BrowserRouter>
     );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,8 +5,12 @@ import { AuthState } from "./AuthState";
 
 export function Login() {
     return (
-        <main>
-            <div>Login component</div>
+        <main className="login-main">
+            <div className="login-box-wrapper">
+                <div className="login-title-welcome">Welcome to <span className="login-title-span">criticalfail</span></div>
+                <div>Login component</div>
+            </div>
+            
         </main>
     );
 }
