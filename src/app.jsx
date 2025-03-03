07abf34f5188d5476ec8f03a89ac812a1df71d56
tyler-bartschi <tylerabartschi@gmail.com>
2025-03-02 20:55:57 -0700

    Created login and create account components

--- src/app.jsx
+++ src/app.jsx
@@ -4,12 +4,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './app.css';
 
 import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+import {Login} from './login/login';
 
 export default function App() {
     return (
         <BrowserRouter>
             <header></header>
-            <div>App goes here</div>
+            <Routes>
+                <Route path="/" element={<Login />} exact />
+            </Routes>
             <footer></footer>
         </BrowserRouter>
     );
