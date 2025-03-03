43b4e73ee6b0485d5f4d749652fed447a71a9b34
tyler-bartschi <tylerabartschi@gmail.com>
2025-03-03 11:51:15 -0700

    Added basic login input

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,11 +4,23 @@ import "./login.css";
 import { AuthState } from "./AuthState";
 
 export function Login() {
+    const [userEmail, setUserEmail] = React.useState(localStorage.getItem('userEmail' || ""));
+    const [userPass, setUserPass] = React.useState("");
+
     return (
         <main className="login-main">
             <div className="login-box-wrapper">
                 <div className="login-title-welcome">Welcome to <span className="login-title-span">criticalfail</span></div>
-                <div>Login component</div>
+                <div className="login-data-wrapper">
+                    <div className="login-email-data">
+                        <span className="login-email-header">Email</span>
+                        <input className="login-email-input" type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} placeholder="Enter email" />
+                    </div>
+                    <div className="login-password-data">
+                        <span className="login-password-header">Password</span>
+                        <input className="login-password-input" type="password" value={userPass} onChange={(e) => setUserPass(e.target.value)} placeholder="Enter password" />
+                    </div>
+                </div>
             </div>
             
         </main>
