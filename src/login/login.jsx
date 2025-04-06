9aa6e766eb9a892137644fde03e537eaaf859d79
tyler-bartschi <tylerabartschi@gmail.com>
2025-04-06 16:09:43 -0600

    Added method for show/hide password

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,21 +4,28 @@ import "./login.css";
 import { AuthState } from "./AuthState";
 
 export function Login() {
-    const [userEmail, setUserEmail] = React.useState(localStorage.getItem('userEmail' || ""));
+    const [userEmail, setUserEmail] = React.useState("");
     const [userPass, setUserPass] = React.useState("");
+    const [showPass, setShowPass] = React.useState(false);
+
 
     return (
         <main className="login-main">
             <div className="login-box-wrapper">
                 <div className="login-title-welcome">Welcome to <span className="login-title-span">criticalfail</span></div>
+                <h3 className="login-title">Login</h3>
                 <div className="login-data-wrapper">
-                    <div className="login-email-data">
-                        <span className="login-email-header">Email</span>
-                        <input className="login-email-input" type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} placeholder="Enter email" />
+                    <div className="login-box-data">
+                        <span className="login-header email-adjuster">Email</span>
+                        <input className="login-input" type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} placeholder="Enter email" />
                     </div>
-                    <div className="login-password-data">
-                        <span className="login-password-header">Password</span>
-                        <input className="login-password-input" type="password" value={userPass} onChange={(e) => setUserPass(e.target.value)} placeholder="Enter password" />
+                    <div className="login-box-data">
+                        <span className="login-header password-adjuster">Password</span>
+                        <input className="login-input" type={showPass ? "text" : "password"} value={userPass} onChange={(e) => setUserPass(e.target.value)} placeholder="Enter password" />
+                    </div>
+                    <div className="show-password-box">
+                        <input type="checkbox" id="show-pass" checked={showPass} onChange={() => {setShowPass(!showPass)}}/>
+                        <span className="show-password-header">Show password</span>
                     </div>
                 </div>
             </div>
