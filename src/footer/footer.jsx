c53a8835673401f2fb749dfe17066d5dfab12fab
tyler-bartschi <tylerabartschi@gmail.com>
2025-04-09 16:38:32 -0600

    Built the footer

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -6,7 +6,14 @@ import {AuthState} from "/src/login/AuthState";
 export function Footer({authType}) {
     if (authType === AuthState.Authenticated) { 
         return (
-            <footer>Test footer</footer>
+            <footer>
+                <img className="footer-image" src="images/dice-footer-image1.png" />
+                {/* <img className="footer-image" src="images/dice-footer-image2.png" /> */}
+                {/* <div className="footer-text">Creator: Tyler</div> */}
+                <a href="https://github.com/tyler-bartschi/criticalfail-source" target="_blank" rel="noopener noreferrer" className='footer-link'>
+                    GitHub Repository
+                </a>
+            </footer>
         );
     }
 }
