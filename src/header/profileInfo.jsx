35face94c78d2e3e420a4f32eb2c5793a56a1b30
tyler-bartschi <tylerabartschi@gmail.com>
2025-04-13 15:33:07 -0600

    made a caret that can toggle

--- src/header/profileInfo.jsx
+++ src/header/profileInfo.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './profileInfo.css';
 
 export function ProfileInfo({username}) {
+    function rotateCaret() {
+        document.getElementById('rotating-caret').classList.toggle('rotate-caret');
+    }
+
     return (
-        <div className="profile-wrapper">
+        <div className="profile-wrapper" onClick={() => rotateCaret()}>
             <img className="profile-image" src="/images/default-profile.png" />
             {username}
-            &#94;
+            {/* <span className="profile-indicator-arrow">&#94;</span>&#9662; */}
+            <img className="caret" id="rotating-caret" src="/images/caret-background-removed.png" />
         </div>
     );
 }
