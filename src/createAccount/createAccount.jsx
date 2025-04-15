ad4064151e2b57342b872733dbe451f0b54c2385
tyler-bartschi <tylerabartschi@gmail.com>
2025-04-14 19:09:28 -0600

    Built a back button for the create account tab

--- src/createAccount/createAccount.jsx
+++ src/createAccount/createAccount.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './createAccount.css';
+import {useNavigate} from "react-router-dom";
 
 export function CreateAccount() {
+    const navigate = useNavigate();
+
     return (
-        <main>
-            <div>Create placeholder</div>
+        <main className="create-main">
+            <div className="create-box-wrapper">
+                <button type="submit" className="create-back-button" onClick={() => navigate('/')}><img className="create-back-arrow" src="/images/caret-background-removed.png" /> Back</button>
+                <div className='create-title'>Create an Account</div>
+            </div>
         </main>
     );
 }
