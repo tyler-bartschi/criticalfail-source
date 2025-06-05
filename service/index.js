004e36d65905c8be529bef485e1f068332e60c6f
tyler-bartschi <tylerabartschi@gmail.com>
2025-06-04 21:51:45 -0600

    Added some notes for myself

--- service/index.js
+++ service/index.js
@@ -8,14 +8,15 @@ const DB = require('./database.js');
 const app = express();
 const authCookieName = 'cookie_token';
 
+// array for existing friend codes already in the system
+// LATER - make a way for this list to persist between deployments. Maybe have a function automatically update this list from the DB?
+const EXISTING_FRIEND_CODES = [];
+
 // tests the connection to the database
 DB.testConnection();
 // DB.testData();
 
 const port = process.argv.length > 2 ? process.argv[2] : 5000;
-
-// array for existing friend codes already in the system
-const EXISTING_FRIEND_CODES = [];
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,29 +25,64 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
 
+// NOTE - I can include next as part of the async parameters, which will tell express to move onto the next matching middleware
+
 apiRouter.post('/auth/user/create', async (req, res) => {
     // creates a new user. Fails for random reasons, and if a user with the same email is found.
     // When successful, will set an authcookie and send the user object to the frontend
     // expects the request body to have an email
     const result = await findUser('email', req.body.email);
     if (result === "error") {
-        res.status(500).send({msg: "An error occured, wait a moment and try again"});
+        sendError(res, 500, "An error occured, wait a moment and try again");
     } else if (result) {
-        res.status(409).send({msg: "Email already in use"});
+        sendError(res, 409, "Email already in use");
     } else {
         const user = await createUser(req.body.email, req.body.username, req.body.password);
 
         if (!user) {
-            res.status(500).send({msg: "Account creation failed. Please try again"});
+            sendError(res, 500, "Account creation failed. Please try again");
         } else {
             setAuthCookie(res, user.cookie_token);
-            // .json automatically stringifies the object and sets the appropriate header
+            // NOTE - .json automatically stringifies the object and sets the appropriate header
             res.status(200).json(user);
         }
     }
 });
 
+app.use(function (err, req, res, next) {
+    // general error handling, can be triggered by calling next(err)
+    res.status(500).send({type: err.name, message: err.message});
+});
+
+app.use((_req, res) => {
+    // generalized middleware, sends the index.html if no middleware is triggered before this
+    res.sendFile('index.html', {root: 'public'});
+});
+
+app.listen(port, () => {
+    // tells express to listen on the specified port
+    console.log(`Listening on port ${port}`);
+});
+
+
+// HELPER FUNCTIONS DEALING WITH RES, REQ, OR DB HANDLING
+
+function setAuthCookie(res, authToken) {
+    // sets authentication cookie for the user
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+function sendError(res, statusCode=500, message="An error occured") {
+    // handles error sending
+    res.status(statusCode).json({error: message});
+}
+
 async function findUser(field, value) {
+    // finds a user by a specified field
     if (!value) return null;
 
     if (field === "email") {
@@ -77,27 +113,8 @@ async function createUser(email, username, password) {
     return false;
 }
 
-function setAuthCookie(res, authToken) {
-    // sets authentication cookie for the user
-    res.cookie(authCookieName, authToken, {
-        secure: true,
-        httpOnly: true,
-        sameSite: 'strict',
-    });
-}
 
-app.use(function (err, req, res, next) {
-    res.status(500).send({type: err.name, message: err.message});
-});
-
-app.use((_req, res) => {
-    res.sendFile('index.html', {root: 'public'});
-});
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
-
+// HELPER FUNCTIONS THAT DO NOT INTERFACE WITH REQ, RES, OR DB
 
 function getRandomInt() {
     // Generates random integer for use in user id field
