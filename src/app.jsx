e4a3a3490f7fc6f886d1de258aa9b56afc45b8e8
tyler-bartschi <tylerabartschi@gmail.com>
2025-06-06 21:48:53 -0600

    Added some basic stuff for an error modal

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './app.css';
 
 import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
