0429a1fd2bb2b6a3a447b73dcda1db4e3e7165bc
tyler-bartschi <tylerabartschi@gmail.com>
2025-06-18 16:43:45 -0600

    started work on reset-password portion

--- src/authentication/reset_password/reset_password.jsx
+++ src/authentication/reset_password/reset_password.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import "./reset_password.css";
 
+import {useNavigate} from 'react-router-dom';
+
 export function ResetPassword() {
+    const navigate = useNavigate();
     return (
-        <div>Testing</div>
+        <main className="reset-main">
+            <div className="reset-body-wrapper">
+                <button type="submit" className="reset-back-button" onClick={() => navigate('/')}><img className="reset-back-arrow" src="/images/caret-background-removed.png"/>Back</button>
+                <div>Reset Password</div>
+                <div>Enter email here</div>
+            </div>
+            <div></div>
+        </main>
     );
 }
